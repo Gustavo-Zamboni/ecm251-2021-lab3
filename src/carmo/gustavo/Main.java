dab398b80545b506c24ba181aeb896e3e58a85ba
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-03-08 13:03:51 -0300

    Aula03

---
+++ src/carmo/gustavo/Main.java
@@ -0,0 +1,47 @@
+package carmo.gustavo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Criar uma referência para conta
+        Conta c1;
+    //Instanciar um objeto conta
+        c1 = new Conta();
+        c1.cliente = new Cliente();
+        c1.cliente.titular = "Inuyasha";
+        c1.saldo = 199.99;
+
+        c1.visualizarSaldo();
+
+        Conta c2 = new Conta();
+        c2.cliente = new Cliente();
+        c2.cliente.titular = "Seichomaru";
+        c2.saldo = 1000;
+        c2.visualizarSaldo();
+
+        c1.depositar(20);
+        c2.depositar(35.99);
+
+        c1.visualizarSaldo();
+        c2.visualizarSaldo();
+
+        if (c1.sacar(100)){
+            System.out.println("Deu bom!");
+        }else {
+            System.out.println("Deu ruim!");
+        }
+        c1.visualizarSaldo();
+
+        if (c1.transferirPara(100,c2)){
+            System.out.println("Transferiu!");
+        }else {
+            System.out.println("Não foi!");
+        }
+
+        c1.visualizarSaldo();
+        c2.visualizarSaldo();
+
+        System.out.println("C1:" + c1.toString());
+        System.out.println("C2:" + c2.toString());
+    }
+}
