d95e8a40dc697e1fafb4023869b97eedf98d35dd
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-03-15 13:01:27 -0300

    Ajuste do programa MauaBank

--- Aula05/src/carmo/gustavo/Conta.java
+++ Aula05/src/carmo/gustavo/Conta.java
@@ -1,25 +1,31 @@
 package carmo.gustavo;
 
 public class Conta {
-    int numero;
-    double saldo;
-    Cliente cliente;
-    void visualizarSaldo(){
-        double saldo = 10;
-        System.out.println("Saldo da conta: R$" + this.saldo);
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(int numero, double saldo, String cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = new Cliente(cliente);
     }
-    void depositar(double valor) {
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+    public void depositar(double valor) {
         this.saldo += valor;
 
     }
-    boolean sacar(double valor) {
+    public boolean sacar(double valor) {
         if (this.saldo >= valor) {
             this.saldo -= valor;
             return true;
         }
         return false;
     }
-    boolean transferirPara(double valor, Conta destino) {
+    public boolean transferirPara(double valor, Conta destino) {
         if (this.sacar(valor)){
             destino.depositar(valor);
             return true;
