b0df8f416ad13136fd6209e44a2713c6f2bef0d2
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-03-18 09:18:52 -0300

    Validar CPF exercício

---
+++ Aula05-teoria/src/carmo/gustavo/Main.java
@@ -0,0 +1,12 @@
+package carmo.gustavo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	String cpf = "505.000.518-36";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+
+    }
+
+}
