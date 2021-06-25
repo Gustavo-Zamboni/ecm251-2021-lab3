e8c2647e949c44b351d1a25e8b0b30b23c80c4db
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-25 17:00:29 -0300

    Implementação das opções de mudança de horário e saída do sistema

--- Atividade2/src/carmo/gustavo/Sistema.java
+++ Atividade2/src/carmo/gustavo/Sistema.java
@@ -1,16 +1,21 @@
 package carmo.gustavo;
 
+import carmo.gustavo.enums.HorarioSistema;
 import carmo.gustavo.interfaces.Auxiliar;
+import carmo.gustavo.models.Membro;
+
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Sistema implements Auxiliar {
+public class Sistema implements Auxiliar{
 
     public void Executar(){
         Scanner ler = new Scanner(System.in);
         String nomeUsuario;
         int idUsuario;
         String categoriaUsuario;
+        ArrayList<Membro> listaMobileMembers= new ArrayList<>();
 
         System.out.println("------Bem vindo ao gerenciador da MAsK_S0c13ty------");
         System.out.println("Insira seus dados:");
@@ -26,16 +31,40 @@ public class Sistema implements Auxiliar {
             System.out.println("Falha ao criar o arquivo!");
         }
         System.out.println("----------------------------------------------------");
-        System.out.println("-----------------------Menu-------------------------");
-        System.out.println("Horário de trabalho: REGULAR");
-        System.out.println("1. Trocar horário do sistema");
-        System.out.println("2. Adicionar membros");
-        System.out.println("3. Postar mensagem dos integrantes");
-        System.out.println("4. Relatório dos membros (apresentação)");
-        System.out.println("5. Sair do Programa");
 
-        
+        int opMenu = 0;
+        HorarioSistema horaSistema= HorarioSistema.REGULAR;
 
+        while (opMenu != 6) {
+            System.out.println("-----------------------Menu-------------------------");
+            System.out.println("Horário de trabalho: " + horaSistema);
+            System.out.println("1. Trocar horário do sistema");
+            System.out.println("2. Adicionar membros");
+            System.out.println("3. Remover membros");
+            System.out.println("4. Postar mensagem dos integrantes");
+            System.out.println("5. Relatório dos membros (apresentação)");
+            System.out.println("6. Sair do Programa");
+            System.out.println("Selecione a opção desejada:");
+            opMenu = ler.nextInt();
+            switch (opMenu) {
+                case (1):
+                    if (horaSistema.equals(HorarioSistema.REGULAR))
+                        horaSistema = HorarioSistema.EXTRA;
+                    else
+                        horaSistema = HorarioSistema.REGULAR;
+                    break;
+                case (2):
+                    System.out.println("Selecione o tipo de membro");
+                    System.out.println("1. Mobile Member");
+                    System.out.println("2. Heavy Lifter");
+                    System.out.println("3. Script Guy");
+                    System.out.println("4. Big Brother");
+                    break;
+                case(6):
+                    System.out.println("Sistema Finalizado!");
+                    break;
+            }
+        }
     }
 
     @Override
