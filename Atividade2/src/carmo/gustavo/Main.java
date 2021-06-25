831bb9d366f067dbf662e926110b94e326c9482d
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-25 14:25:27 -0300

    Testes de comunicação entre main e sistema

--- Atividade2/src/carmo/gustavo/Main.java
+++ Atividade2/src/carmo/gustavo/Main.java
@@ -7,6 +7,7 @@ package carmo.gustavo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Sistema sistema = new Sistema();
+	sistema.Executar();
     }
 }
--- Atividade2/src/carmo/gustavo/Sistema.java
+++ Atividade2/src/carmo/gustavo/Sistema.java
@@ -1,5 +1,7 @@
 package carmo.gustavo;
 
 public class Sistema {
-    
+    public void Executar(){
+        System.out.println("Funcionando!");
+    }
 }
