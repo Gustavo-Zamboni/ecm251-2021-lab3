c0f65adaedc70d92e56ad0e55e1506ce2e8536f2
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-25 14:20:17 -0300

    Inicialização do trabalho
    Nome, RA

---
+++ Atividade2/src/carmo/gustavo/Main.java
@@ -0,0 +1,12 @@
+// Nome: Gustavo Zamboni do Carmo
+// RA: 19.01266-7
+
+package carmo.gustavo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
