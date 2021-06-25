579f44f26c6b67f7e14ad2d87e3b871b702ecaf5
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-25 16:27:18 -0300

    Implementação da escrita do arquivo em csv

--- Atividade2/src/carmo/gustavo/Sistema.java
+++ Atividade2/src/carmo/gustavo/Sistema.java
@@ -1,9 +1,14 @@
 package carmo.gustavo;
 
+import carmo.gustavo.enums.TiposMembros;
 import carmo.gustavo.interfaces.Auxiliar;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Sistema implements Auxiliar {
+
     public void Executar(){
         Scanner ler = new Scanner(System.in);
         String nomeUsuario = "";
@@ -18,7 +23,18 @@ public class Sistema implements Auxiliar {
         categoriaUsuario = ler.next();
         System.out.println("id:");
         idUsuario = ler.nextInt();
-
+        try {
+            criarArquivo(categoriaUsuario,nomeUsuario,idUsuario);
+        } catch (Exception e) {
+            System.out.println("Falha ao criar o arquivo!");
         }
+    }
 
+    @Override
+    public void criarArquivo(String categoria, String nome, int id) throws Exception {
+        FileWriter escreveArquivo = new FileWriter("arquivo_super_Secreto_nao_abrir.csv");
+        String dadosUsuario = categoria + ";" + nome + ";" + Integer.toString(id);
+        escreveArquivo.write(dadosUsuario);
+        escreveArquivo.close();
+    }
 }
--- Atividade2/src/carmo/gustavo/interfaces/Auxiliar.java
+++ Atividade2/src/carmo/gustavo/interfaces/Auxiliar.java
@@ -1,15 +1,6 @@
 package carmo.gustavo.interfaces;
 
-import carmo.gustavo.enums.TiposMembros;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public interface Auxiliar {
-    public static void criarArquivo(TiposMembros categoria, String nome, int id) throws IOException {
-        FileWriter escreveArquivo = new FileWriter("arquivo_super_Secreto_nao_abrir.csv");
-        String dadosUsuario = categoria + ";" + nome + ";" + Integer.toString(id);
-        escreveArquivo.write(dadosUsuario);
-
-        escreveArquivo.close();
-    }
+    void criarArquivo(String categoria, String nome, int id) throws Exception;
 }
