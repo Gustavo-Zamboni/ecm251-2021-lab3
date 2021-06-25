6e4b3528f22b55e9b958656eb671449334cb95d7
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-25 17:25:23 -0300

    Implementação da funcionalidade de adicionar membros

--- Atividade2/src/carmo/gustavo/Sistema.java
+++ Atividade2/src/carmo/gustavo/Sistema.java
@@ -1,8 +1,11 @@
 package carmo.gustavo;
 
 import carmo.gustavo.enums.HorarioSistema;
+import carmo.gustavo.enums.TiposMembros;
 import carmo.gustavo.interfaces.Auxiliar;
+import carmo.gustavo.models.HeavyLifter;
 import carmo.gustavo.models.Membro;
+import carmo.gustavo.models.MobileMember;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
@@ -16,6 +19,9 @@ public class Sistema implements Auxiliar{
         int idUsuario;
         String categoriaUsuario;
         ArrayList<Membro> listaMobileMembers= new ArrayList<>();
+        ArrayList<Membro> listaHeavyLifters = new ArrayList<>();
+        ArrayList<Membro> listaScriptGuys = new ArrayList<>();
+
 
         System.out.println("------Bem vindo ao gerenciador da MAsK_S0c13ty------");
         System.out.println("Insira seus dados:");
@@ -34,6 +40,12 @@ public class Sistema implements Auxiliar{
 
         int opMenu = 0;
         HorarioSistema horaSistema= HorarioSistema.REGULAR;
+        int tipoMembro;
+        String nomeMembro;
+        String emailMembro;
+        TiposMembros funcaoMembro;
+        int idMembro;
+
 
         while (opMenu != 6) {
             System.out.println("-----------------------Menu-------------------------");
@@ -59,9 +71,51 @@ public class Sistema implements Auxiliar{
                     System.out.println("2. Heavy Lifter");
                     System.out.println("3. Script Guy");
                     System.out.println("4. Big Brother");
+                    System.out.println("Tipo:");
+                    tipoMembro = ler.nextInt();
+                    switch (tipoMembro){
+                        case (1):
+                            funcaoMembro = TiposMembros.MOBILEMEMBER;
+                            System.out.println("Nome(sem espaços):");
+                            nomeMembro = ler.next();
+                            System.out.println("Email:");
+                            emailMembro = ler.next();
+                            System.out.println("Id:");
+                            idMembro = ler.nextInt();
+                            Membro mobileMember = new MobileMember(nomeMembro,emailMembro,idMembro);
+                            listaMobileMembers.add(mobileMember);
+                            //for(Membro membro : listaMobileMembers) {
+                            //    System.out.println(membro.apresentacao());
+                            //}
+                            break;
+                        case (2):
+                            funcaoMembro = TiposMembros.HEAVYLIFTER;
+                            System.out.println("Nome(sem espaços):");
+                            nomeMembro = ler.next();
+                            System.out.println("Email:");
+                            emailMembro = ler.next();
+                            System.out.println("Id:");
+                            idMembro = ler.nextInt();
+                            Membro heavyLifter = new HeavyLifter(nomeMembro,emailMembro,idMembro);
+                            listaHeavyLifters.add(heavyLifter);
+                            break;
+                        case(3):
+
+                            break;
+                        case(4):
+                            break;
+                        default:
+                            System.out.println("Número inválido!");
+                            break;
+                    }
+                    break;
+                case(3):
                     break;
                 case(6):
                     System.out.println("Sistema Finalizado!");
+                    break;
+                default:
+                    System.out.println("Funcionalidade ainda não implementada!");
                     break;
             }
         }
