3d9ac72d64dcc9277558851b7345e3b029613a1e
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-26 16:56:08 -0300

    Documentação da classe enum HorarioSistema

--- Atividade2/src/carmo/gustavo/enums/HorarioSistema.java
+++ Atividade2/src/carmo/gustavo/enums/HorarioSistema.java
@@ -1,5 +1,8 @@
 package carmo.gustavo.enums;
 
+/**
+ * Classe do tipo "enum" para determinar variaveis que representam os dois possiveis horarios do sistema, que sao Regular e Extra
+ */
 public enum HorarioSistema {
     REGULAR, EXTRA;
 }
