2fc8f6755836879f9b13b04b00726dc55d2745aa
Gustavo-Zamboni <79462541+Gustavo-Zamboni@users.noreply.github.com>
2021-06-26 16:58:46 -0300

    Documentação da classe enum TiposMembros

--- Atividade2/src/carmo/gustavo/enums/HorarioSistema.java
+++ Atividade2/src/carmo/gustavo/enums/HorarioSistema.java
@@ -1,7 +1,8 @@
 package carmo.gustavo.enums;
 
 /**
- * Classe do tipo "enum" para determinar variaveis que representam os dois possiveis horarios do sistema, que sao Regular e Extra
+ * Classe do tipo "enum" para determinar variaveis que representam
+ * os dois possiveis horarios do sistema, que sao Regular e Extra
  */
 public enum HorarioSistema {
     REGULAR, EXTRA;
--- Atividade2/src/carmo/gustavo/enums/TiposMembros.java
+++ Atividade2/src/carmo/gustavo/enums/TiposMembros.java
@@ -1,5 +1,10 @@
 package carmo.gustavo.enums;
 
+/**
+ * Classe do tipo "enum" para determinar os
+ * possiveis cargos dos membros da Mask Society, que sao:
+ * Mobile Member, Heavy Lifter, Script Guy e Big Brother
+ */
 public enum TiposMembros {
     MOBILEMEMBER, HEAVYLIFTER, SCRIPTGUY, BIGBROTHER;
 }
